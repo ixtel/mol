979186002df9de8861e40ef583321083588643f3
nin-jin <nin-jin@ya.ru>
2016-01-23 17:05:15 +0300

    Spoiler simplification

--- spoiler/spoiler.view.ts
+++ spoiler/spoiler.view.ts
@@ -2,9 +2,6 @@
 class $mol_spoiler extends $mol.$mol_spoiler {
     
     @ $jin2_grab
-    expanded() { return new $jin2_atom( () => false ) }
-    
-    @ $jin2_grab
 	child() { return new $jin2_atom( () => [
         this.switcher().get() ,
         this.expanded().get() ? this.contenter().get() : null ,
