193c12281643233f9c94df12fe39711777eb9ec1
nin-jin <nin-jin@ya.ru>
2016-02-06 23:33:52 +0300

    +fix

--- switcher/switcher.view.ts
+++ switcher/switcher.view.ts
@@ -5,7 +5,7 @@ class $mol_switcher extends $mol.$mol_switcher {
 	checked() { return new $jin2_atom(
 		() => this.selected().get() === this.value().get() ,
 		next => {
-			this.selected().set( next ? this.value().get() : '' )
+			this.selected().set( next ? this.value().get() : null )
 			return next
 		}
 	) }
