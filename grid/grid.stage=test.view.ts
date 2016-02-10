ed831106ce5cf17e6254aa0759cdceec3294c1c1
nin-jin <nin-jin@ya.ru>
2016-02-11 01:04:14 +0300

    + grid

---
+++ grid/grid.stage=test.view.ts
@@ -0,0 +1,33 @@
+@ $mol_replace
+class $mol_grid_demo extends $mol.$mol_grid_demo {
+	
+	@ $jin2_grab
+	rows() { return new $jin2_atom( () => {
+		var rows = []
+		rows.push( this.header().get() )
+		for( var i = 0 ; i < 40 ; ++i ) {
+			rows.push( this.row( i ).get() )
+		}
+		return rows
+	} ) }
+
+	@ $jin2_grab
+	row( id : number ) { return new $jin2_atom_own( () => {
+		var view = new $mol_grid_demo_row
+		return view
+	} ) }
+	
+}
+
+@ $mol_replace
+class $mol_grid_demo_row extends $mol.$mol_grid_demo_row {
+	
+	@ $jin2_grab
+	id() { return new $jin2_atom( () => this.objectOwner.objectId ) }
+
+	@ $jin2_grab
+	stringContent() { return new $jin2_atom(
+		() => 'Hello, Mister ' + '00000000000000000000'.substring( Math.round( Math.random() * 20 ) )
+	) }
+
+}
