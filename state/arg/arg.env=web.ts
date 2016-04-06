14bea1ba910ea9b9fb61b07b416ceaf40205a0c7
nin-jin <nin-jin@ya.ru>
2016-04-06 11:19:28 +0300

    tabify

--- state/arg/arg.env=web.ts
+++ state/arg/arg.env=web.ts
@@ -2,12 +2,12 @@ class $mol_state_arg extends $jin2_atom<string> {
 
 	key() { return new $jin2_prop( '' ) }
 
-    item( key : string ) {
+	item( key : string ) {
 		var prefix = this.key().get()
 		return $mol_state_arg.item( prefix ? ( prefix + '.' + key ) : key )
-    }
+	}
 	
-    @ $jin2_grab
+	@ $jin2_grab
 	link( next : string ) {
 		return new $jin2_prop( () => $mol_state_arg.make({ [ this.key().get() ] : next }) )
 	}
@@ -21,63 +21,63 @@ class $mol_state_arg extends $jin2_atom<string> {
 		return next
 	}
 
-    @ $jin2_grab
-    static item( key? : string ) {
+	@ $jin2_grab
+	static item( key? : string ) {
 		return (new this).setup( _ => {
 			_.key = () => new $jin2_prop( key ) 
 		} )
-    }
+	}
 
-    @ $jin2_grab
-    static href() {
-        return new $jin2_atom(
-            () => window.location.search + window.location.hash,
-            next => document.location.href = next
-        )
-    }
+	@ $jin2_grab
+	static href() {
+		return new $jin2_atom(
+			() => window.location.search + window.location.hash,
+			next => document.location.href = next
+		)
+	}
 
-    @ $jin2_grab
-    static dict() {
-        return new $jin2_atom(
-            () => {
-                var href = this.href().get()
-                var chunks = href.split( /[\/\?#!&;]/g )
-                var params = <{ [ index : string ] : string }> {}
-                chunks.forEach( chunk => {
-                    if( !chunk ) return
-                    var vals = chunk.split( /[:=]/ ).map( decodeURIComponent )
-                    params[ vals[0] ] = vals[1] || ''
-                })
-                return params
-            },
-            next  => {
-                this.href().set( this.make( next ) )
-            }
-        )
-    }
+	@ $jin2_grab
+	static dict() {
+		return new $jin2_atom(
+			() => {
+				var href = this.href().get()
+				var chunks = href.split( /[\/\?#!&;]/g )
+				var params = <{ [ index : string ] : string }> {}
+				chunks.forEach( chunk => {
+					if( !chunk ) return
+					var vals = chunk.split( /[:=]/ ).map( decodeURIComponent )
+					params[ vals[0] ] = vals[1] || ''
+				})
+				return params
+			},
+			next  => {
+				this.href().set( this.make( next ) )
+			}
+		)
+	}
 
-    static make( next : { [ index : string ] : any } ) {
-        var params = {}
+	static make( next : { [ index : string ] : any } ) {
+		var params = {}
 
-        var prev = this.dict().get()
-        for( var key in prev ) {
-            if( key in next ) continue
-            params[ key ] = prev[ key ]
-        }
+		var prev = this.dict().get()
+		for( var key in prev ) {
+			if( key in next ) continue
+			params[ key ] = prev[ key ]
+		}
 
-        for( var key in next ) {
-            params[ key ] = next[ key ]
-        }
+		for( var key in next ) {
+			params[ key ] = next[ key ]
+		}
 
-        var chunks = []
-        for( var key in params ) {
-            if( null == params[key] ) continue
-            chunks.push( [ key ].concat( params[key] ).map( encodeURIComponent ).join( '=' ) )
-        }
+		var chunks = []
+		for( var key in params ) {
+			if( null == params[key] ) continue
+			chunks.push( [ key ].concat( params[key] ).map( encodeURIComponent ).join( '=' ) )
+		}
 		chunks.sort()
 
-        return '#' + chunks.join( '#' )
-    }
+		return '#' + chunks.join( '#' )
+	}
 	
 }
 
